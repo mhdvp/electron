28cfd48935cb65dcff327236c32a9000820a7797
Niklas Wenzel <dev@nikwen.de>
2026-04-20 18:57:56 +0200

    feat: support heap profiling in `contentTracing`

--- shell/app/electron_content_client.h
+++ shell/app/electron_content_client.h
@@ -33,6 +33,9 @@ class ElectronContentClient : public content::ContentClient {
   void AddContentDecryptionModules(
       std::vector<content::CdmInfo>* cdms,
       std::vector<media::CdmHostFilePath>* cdm_host_file_paths) override;
+  void ExposeInterfacesToBrowser(
+      scoped_refptr<base::SequencedTaskRunner> io_task_runner,
+      mojo::BinderMap* binders) override;
 };
 
 }  // namespace electron
